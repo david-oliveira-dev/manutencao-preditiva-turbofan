4c9647de2f024591f531cfae24fb5251c91c45a0
David Oliveira <davidoliveira.devbr@gmail.com>
2026-06-22 09:27:46 -0300

    chore: scaffold do projeto (estrutura, gitignore, requirements)

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""Manutenção preditiva de motores turbofan (NASA C-MAPSS).
+
+Prevê a Vida Útil Restante (RUL) a partir de leituras de sensores.
+Pipeline: data -> features -> model.
+"""
+
+__version__ = "0.1.0"
